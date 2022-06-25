0ece9f82c4503ba82102153cd7f4d644bb84a04e
EdMadd <martl.eichinger@gmail.com>
2022-06-25 08:56:35 +0200

    init

--- src/components/Details/Details.tsx
+++ src/components/Details/Details.tsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import { time } from 'console';
 import { useEffect, useState } from 'react';
 import { useParams } from 'react-router-dom';
 import { getData } from '../../store/api';
@@ -30,17 +31,24 @@ const Details: React.FC<IDetailsProps> = ({ color }) => {
   console.log('Details', mediaId);
 
   return (
-    <MediaDetail>
-      <h1>{mediaDetail?.title}</h1>
+    <MediaDetail color={color}>
       <div
         className="bg"
         style={{ backgroundImage: `url(${imgURL + '/' + mediaDetail?.backdrop_path})` }}
       ></div>
       <div className="fg d-flex flex-row">
         <img src={imgURL + '/' + mediaDetail?.poster_path} alt="foto" />
-        <div className="d-flex flex-column">
-          <p>...{mediaDetail?.tagline}...</p>
-          <h3>Overview</h3>
+        <div className="fg-details d-flex flex-column">
+          <h1>{mediaDetail?.title}</h1>
+          <h3 className="normal">({mediaDetail?.release_date.split('-')[0]})</h3>
+          <h5>
+            {mediaDetail?.genres.map((item: any) => {
+              return item.name + ', ';
+            })}
+            Vote: {mediaDetail?.vote_average}
+          </h5>
+          <h4 className="styled">{mediaDetail?.tagline}</h4>
+          <h4>Overview</h4>
           <p>{mediaDetail?.overview}</p>
         </div>
       </div>
@@ -50,7 +58,11 @@ const Details: React.FC<IDetailsProps> = ({ color }) => {
 
 export default Details;
 
-const MediaDetail = styled.div`
+type dashCSSProps = {
+  color: any;
+};
+
+const MediaDetail = styled.div<dashCSSProps>`
   h1 {
     margin-bottom: 32px;
   }
@@ -70,5 +82,32 @@ const MediaDetail = styled.div`
     padding: 32px;
     position: relative;
     z-index: 1;
+
+    img {
+      max-width: 40%;
+    }
+
+    .fg-details {
+      padding: 32px;
+
+      h1 {
+        margin-bottom: 8px;
+      }
+
+      .normal {
+        font-weight: 100;
+        margin-bottom: 32px;
+        color: ${({ color }) => color.greyishBlue};
+      }
+
+      .styled {
+        font-weight: 100;
+        font-size: 24px;
+        margin-top: 24px;
+        margin-bottom: 24px;
+        font-style: italic;
+        color: ${({ color }) => color.greyishBlue};
+      }
+    }
   }
 `;
